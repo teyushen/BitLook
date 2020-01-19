371ab900b500db22f9109db2298b0f64ff6467be
Dennis <teyushen@gmail.com>
2020-01-19 12:33:18 +0800

    BitMEX sample

---
+++ settings.js
@@ -0,0 +1,27 @@
+let testnet = {
+    BTSE: {
+        btseUrl: 'https://testnet.btse.io',
+        btseApi: '',
+        btseSecret: ''
+    },
+    BitMEX: {
+        bitMEXUrl: 'https://testnet.bitmex.com',
+        bitMEXApi: '',
+        bitMEXSecret: ''
+    }
+};
+
+let prod = {
+    BTSE: {
+        btseUrl: 'https://api.btse.com',
+        btseApi: '',
+        btseSecret: ''
+    },
+    BitMEX: {
+        bitMEXUrl: 'https://www.bitmex.com',
+        bitMEXApi: '',
+        bitMEXSecret: ''
+    }
+};
+
+module.exports = testnet;
